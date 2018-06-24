13d992e5179df09d923fd1321b310ae981b0dcfc
wolf637 <alexey.volkov@live.com>
2018-06-24 10:19:32 -0700

    Add Blog functionality

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:150]
+
+    def date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
+    def __str__(self):
+        return self.title
